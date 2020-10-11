95e832b45f4c84c5fdebff66c0a96f5881e72e59
Bayramgeldi <bnurberdiyev@colotek.com>
2020-10-10 21:03:52 -0400

    Updates

---
+++ precache-manifest.266321addd3ad8aaea4c5e66e40f5831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4f23d559c1f2194f6129cc7e165a59",
+    "url": "/evernote-app/index.html"
+  },
+  {
+    "revision": "8f48aad4a15b966b922c",
+    "url": "/evernote-app/static/css/main.915eb1ab.chunk.css"
+  },
+  {
+    "revision": "7b6b11afae34993ba578",
+    "url": "/evernote-app/static/js/2.7aa6ec27.chunk.js"
+  },
+  {
+    "revision": "05f71c02cec75ecbfbabc8e781658c6c",
+    "url": "/evernote-app/static/js/2.7aa6ec27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f48aad4a15b966b922c",
+    "url": "/evernote-app/static/js/main.cb88e8cc.chunk.js"
+  },
+  {
+    "revision": "2f321f51c46f261618ec",
+    "url": "/evernote-app/static/js/runtime-main.f247536a.js"
+  }
+]);
